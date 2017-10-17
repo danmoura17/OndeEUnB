9de783852744310c25876fcf59f7b82bf89c350a
Kairon Velozo <kairon333@gmail.com>
2017-10-16 23:23:48 -0200

    Getting course schedule data.

--- darcyWeb/app/interactors/download_schedules.rb
+++ darcyWeb/app/interactors/download_schedules.rb
@@ -7,18 +7,20 @@ class DownloadSchedules
 
     uri = open(context.url)
     html = Nokogiri::HTML(uri, nil, Encoding::UTF_8.to_s)
-    classes = html.css('tbody tr')
+    classes = html.css('table#datatable')
+
+    classes.shift
 
     # Getting the time and places of courses
     classes.each do |class_|
-      puts class_.content
+      #puts class_.content
       # next unless schedule_row.content.include? 'Total'
-      # classroom = schedule_row.at_css('td[1] b')
-      # schedule_row.css('td[4] div').each do |schedule|
-      #     day_of_week = schedule.at_css('b')
-      #     start_time = schedule.at_css('font[color="black"] b')
-      #     end_time = schedule.at_css('font[color="brown"]')
-      #     room = schedule.at_css('i')
+      class_name = class_.at_css('.turma').text
+      class_.css('td[4] table').each do |schedule_data|
+        day_of_week = schedule_data.at('td[1]').text
+        start_time = schedule_data.at('td[2]').text
+        end_time = schedule_data.at('td[3]').text
+        room = schedule_data.at('tr[2] td[2]').text
       #
       #     next unless valid_schedule_and_room?(day_of_week, start_time, end_time, room, classroom)
       #     room = clean_room_name(room.text.strip)
@@ -27,21 +29,21 @@ class DownloadSchedules
       #
       #     next unless allowed_buildings.include? building
       #
-      #     schedule_data = {
-      #       course: course,
-      #       day_of_week: set_day_of_week(day_of_week.text),
-      #       start_time: start_time.text,
-      #       end_time: end_time.text,
-      #       building: building,
-      #       room: room,
-      #       room_type: define_room_type(room),
-      #       classroom: classroom.text
-      #     }
+        schedule = {
+          # course: course,
+          #day_of_week: set_day_of_week(day_of_week.text),
+          start_time: start_time,
+          end_time: end_time,
+          # building: building,
+          room: room,
+          #room_type: define_room_type(room),
+          #classroom: classroom.text
+        }
       #
       #     puts schedule_data
       #     create_schedule(schedule_data)
       #   end
-      # end
+      end
     end
  end
 end
